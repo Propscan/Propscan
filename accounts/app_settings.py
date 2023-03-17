6c0f2fb62418b3b54f53ee5226c93518a7542c71
satwik2711 <satwik3.14159@gmail.com>
2023-03-18 02:06:27 +0530

    added user type models

---
+++ accounts/app_settings.py
@@ -0,0 +1,16 @@
+USER_TYPE_CHOICES = (
+    ("BUYER","BUYER"),
+    ("OWNER","OWNER"),
+    ("BROKER","BROKER")
+)
+
+RERA_REGISTERED_CHOICES = (
+    (1,"Yes"),
+    (2,"I have applied"),
+    (3,"Not Applicable")
+)
+
+LICENSE_TYPE_CHOICES = (
+    (1, "Individual"),
+    (2, "Firm")
+)
---
+++ accounts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .app_settings import LICENSE_TYPE_CHOICES, USER_TYPE_CHOICES, RERA_REGISTERED_CHOICES
+
+# Create your models here.
+
+class PropScanUser(AbstractUser):
+    phone_no = models.CharField(max_length=30)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default="BUYER",null=False)
+    email_id = models.EmailField(max_length=254)
+    def __str__(self):
+        return self.phone_no
+    
+class Buyer(models.Model):
+    user = models.OneToOneField(PropScanUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=256)
+    def __str__(self):
+        return self.full_name
+    
+class Owner(models.Model):
+    user = models.OneToOneField(PropScanUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=256)
+    additional_phone_no = models.CharField(max_length=30)
+    def __str__(self):
+        return self.full_name
+    
+class Broker(models.Model):
+    user = models.OneToOneField(PropScanUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=256)
+    rera_registered = models.CharField(max_length=10, choices=RERA_REGISTERED_CHOICES)
+    license_type = models.CharField(max_length=20, choices=LICENSE_TYPE_CHOICES)
+    company_name = models.CharField(max_length=100)
+    company_url = models.CharField(max_length=100, null=True)
+    company_address_1 = models.CharField(max_length=256)
+    company_address_2 = models.CharField(max_length=256, null=True)
+    city = models.CharField(max_length=50)
+    description = models.CharField(max_length=256)
+    additional_phone_no_1 = models.CharField(max_length=30, null=True)
+    additional_phone_no_2 = models.CharField(max_length=30, null=True)
+    landline_number_1 = models.CharField(max_length=30, null=True)
+    landline_number_2 = models.CharField(max_length=30, null=True)
+    def __str__(self):
+        return self.full_name
