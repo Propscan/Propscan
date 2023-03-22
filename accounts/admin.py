7c59af7898987e07e19863d0c3df900032057106
harsh dhariwal <89967721+Harsh-dhariwal@users.noreply.github.com>
2023-03-22 19:12:59 +0530

    registered models and added migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import PropScanUser
+from .models import PropScanUser,Buyer,Owner,Broker
 from django.contrib.auth.admin import UserAdmin
+from django.apps import apps
 
-# Register your models here.
+
 admin.site.register(PropScanUser, UserAdmin)
+admin.site.register(Buyer)
+admin.site.register(Owner)
+admin.site.register(Broker)
+
