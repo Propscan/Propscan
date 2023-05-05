ccb3f68ea5a6385b0b84aa0ce2c853df70f8db87
satwik2711 <satwik3.14159@gmail.com>
2023-05-06 00:48:58 +0530

    added: more fields in filters for search page

--- property_listing/filters.py
+++ property_listing/filters.py
@@ -3,25 +3,52 @@ import django_filters
 from .models import PropertyType1, PropertyType2, PropertyType3
 
 class PropertyType1Filter(django_filters.FilterSet):
+    locality = django_filters.CharFilter(field_name="locality", lookup_expr='icontains')
+    locality_society = django_filters.CharFilter(field_name="locality_society", lookup_expr='icontains')
+    sub_locality = django_filters.CharFilter(field_name="sub_locality", lookup_expr='icontains')
+    location_advantages = django_filters.CharFilter(field_name="location_advantages", lookup_expr='icontains')
+
     class Meta:
         model = PropertyType1
         fields = {
             'listing_type': ['exact', 'icontains'],
             'user': ['exact'],
+            'locality': ['exact', 'icontains'],
+            'locality_society': ['exact', 'icontains'],
+            'sub_locality': ['exact', 'icontains'],
+            'location_advantages': ['exact', 'icontains'],
         }
 
 class PropertyType2Filter(django_filters.FilterSet):
+    locality = django_filters.CharFilter(field_name="locality", lookup_expr='icontains')
+    locality_society = django_filters.CharFilter(field_name="locality_society", lookup_expr='icontains')
+    sub_locality = django_filters.CharFilter(field_name="sub_locality", lookup_expr='icontains')
+    location_advantages = django_filters.CharFilter(field_name="location_advantages", lookup_expr='icontains')
+
     class Meta:
         model = PropertyType2
         fields = {
             'listing_type': ['exact', 'icontains'],
             'user': ['exact'],
+            'locality': ['exact', 'icontains'],
+            'locality_society': ['exact', 'icontains'],
+            'sub_locality': ['exact', 'icontains'],
+            'location_advantages': ['exact', 'icontains'],
         }
 
 class PropertyType3Filter(django_filters.FilterSet):
+    locality = django_filters.CharFilter(field_name="locality", lookup_expr='icontains')
+    locality_society = django_filters.CharFilter(field_name="locality_society", lookup_expr='icontains')
+    sub_locality = django_filters.CharFilter(field_name="sub_locality", lookup_expr='icontains')
+    location_advantages = django_filters.CharFilter(field_name="location_advantages", lookup_expr='icontains')
+
     class Meta:
         model = PropertyType3
         fields = {
             'listing_type': ['exact', 'icontains'],
             'user': ['exact'],
+            'locality': ['exact', 'icontains'],
+            'locality_society': ['exact', 'icontains'],
+            'sub_locality': ['exact', 'icontains'],
+            'location_advantages': ['exact', 'icontains'],
         }
