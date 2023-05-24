7bbe0194c82a795c56f5c5ecf2cb01f0621555b6
Mayuri Gupta <mg153@snu.edu.in>
2023-05-24 16:22:43 +0530

    if applied, home page has been pushed

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './assets/App.css';
 import Home from "./pages/Home";
+import PropertyListing from "./pages/PropertyListing"
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/properties" element={<PropertyListing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
 import Blank from "../components/Blank";
+import Header from '../components/header';
 import Metrics from '../components/metrics/Metrics';
 import Properties from "../components/properties/Properties";
 import Info from "../components/info/info";
 import Features from "../components/features/features";
 import Gallery from '../components/gallery/gallery';
 import Clients from '../components/clients/clients';
+import Experts from '../components/experts/experts';
 
 import Footer from '../components/footer/Footer';
 
 export default function Home() {
   return (
     <div>
-        Some Data 
+        <Header />
+        <Blank />
         <Blank />
         <Metrics />
         <Blank />
@@ -22,6 +25,8 @@ export default function Home() {
         <Info />
         <Blank />
         <Features />
+        <Blank />
+        <Experts />
         <Blank />
         <Gallery />
         <Clients />
