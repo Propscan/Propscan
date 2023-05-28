74619a98f97de67a93a82f687d0588e270817cc2
Mayuri Gupta <mg153@snu.edu.in>
2023-05-28 22:15:27 +0530

    if applied, adding property listing

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './assets/App.css';
 import Home from "./pages/Home";
-import PropertyListing from "./pages/PropertyListing"
+import PropertyListingHome from "./pages/PropertyListingHome"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/properties" element={<PropertyListing />} />
+        <Route path="/properties" element={<PropertyListingHome />} />
       </Routes>
     </BrowserRouter>
   );
--- ui/src/pages/PropertyListing.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PropertyListing() {
-  return (
-    <div>
-        Soon a mgic will happen
-    </div>
-  )
-}
